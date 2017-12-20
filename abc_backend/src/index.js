437f07707d81f384878a230c692f3665e299d69f
RRR.tg <davidruizy1983@yandex.com>
2017-12-20 18:17:52 +0800

    babel es6 koa seed project

--- abc_backend/src/index.js
+++ abc_backend/src/index.js
@@ -1,28 +1,21 @@
-require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import dotenv from 'dotenv';
 
-const Koa = require('koa');
-const Router = require('koa-router');
-
-// api handler
-const api = require('./api');
-// const db = require('./db');
-// const res1 = db.run('SELECT * FROM datastore_user');
-
-// load environment variables
-const {
-  PORT: port
-} = process.env;
+dotenv.config(); // LOAD CONFIG
 
 // create app for koa
 const app = new Koa();
 const router = new Router();
 
 // set route for api
-router.use('/api', api.routes());
+// router.use('/api', api.routes());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+const port = process.env.PORT || 3000;
+
 // listen
 app.listen(port, () => {
-  console.log(`PulsePro is listening to port ${port}`);
+  console.log(`ABC is listening to port ${port}`);
 });
